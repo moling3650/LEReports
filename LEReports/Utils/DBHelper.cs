0da4839bfe84125c318a3a7575d79b7f839a4e5f
moling <365024424@qq.com>
2018-03-22 09:36:18 +0800

    添加DBHelper工具类

---
+++ LEReports/Utils/DBHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace LEReports.Utils
+{
+    public class DBHelper
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        public DBHelper()
+        {
+        }
+
+        public static DataTable RunSQL(string sql, object parameters = null)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = sql;
+                    if (parameters != null)
+                    {
+                        foreach (PropertyInfo prop in parameters.GetType().GetProperties())
+                        {
+                            cmd.Parameters.AddWithValue("@" + prop.Name, prop.GetValue(parameters, null) ?? DBNull.Value);
+                        }
+                    }
+                    conn.Open();
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "ds");                    
+                    return ds.Tables["ds"];
+                }
+            }
+        }
+    }
+}

98e1c40dbba38386fd62094dc48a6d6364cc4547
moling <365024424@qq.com>
2018-03-27 15:35:49 +0800

    重构执行sql的方法

--- LEReports/Utils/DBHelper.cs
+++ LEReports/Utils/DBHelper.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Reflection;
+using Newtonsoft.Json.Linq;
 
 namespace LEReports.Utils
 {
@@ -13,19 +14,39 @@ namespace LEReports.Utils
         {
         }
 
-        public static DataTable RunSQL(string sql, object parameters = null)
+        public static DataTable getDataTable(string sql, JToken parameters = null, bool isProcedure = false)
         {
             using (var conn = new SqlConnection(connectionString))
             {
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandType = isProcedure ? CommandType.StoredProcedure : CommandType.Text;
                     cmd.CommandText = sql;
                     if (parameters != null)
                     {
-                        foreach (PropertyInfo prop in parameters.GetType().GetProperties())
+                        foreach (JProperty item in parameters)
                         {
-                            cmd.Parameters.AddWithValue("@" + prop.Name, prop.GetValue(parameters, null) ?? DBNull.Value);
+                            switch (item.Value.Type)
+                            {
+                                case JTokenType.Boolean:
+                                    break;
+                                case JTokenType.Date:
+                                    var dateValue = DateTime.SpecifyKind(item.Value.ToObject<DateTime>(), DateTimeKind.Utc).ToLocalTime();
+                                    cmd.Parameters.AddWithValue("@" + item.Name, dateValue);
+                                    break;
+                                case JTokenType.Float:
+                                    cmd.Parameters.AddWithValue("@" + item.Name, item.Value.ToObject<decimal>());
+                                    break;
+                                case JTokenType.Integer:
+                                    cmd.Parameters.AddWithValue("@" + item.Name, item.Value.ToObject<int>());
+                                    break;
+                                case JTokenType.String:
+                                    cmd.Parameters.AddWithValue("@" + item.Name, item.Value.ToString());
+                                    break;
+                                default:
+                                    cmd.Parameters.AddWithValue("@" + item.Name, DBNull.Value);
+                                    break;
+                            }
                         }
                     }
                     conn.Open();
